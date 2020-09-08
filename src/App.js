5b9f96297450d2572f3372249831f61caaa9f85c
Faustino Loeza Pérez <faustinoloezaperez@gmail.com>
2020-09-08 18:44:43 -0500

    primer cambio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( 
+        <div className='main'>
+            <h1 className='title'> Buscar Pokemon </h1> 
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th className='th'>Nombre</th>
+                        <th className='th'>Tipo</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Pikachu</td>
+                    <td>Bolbasur</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
 export default App;
